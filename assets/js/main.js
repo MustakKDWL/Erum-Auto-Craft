ab6669fdf0fa80116b4c9d9081552a0992aa0251
MustakKDWL <your-email@example.com>
2026-01-23 12:20:32 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+// const navSlide = () => {
+//     const burger = document.querySelector('.burger');
+//     const nav = document.querySelector('.nav-links');
+//     const navLinks = document.querySelectorAll('.nav-links li');
+
+//     burger.addEventListener('click', () => {
+//         nav.classList.toggle('nav-active');
+
+//         navLinks.forEach((link, index) => {
+//             if (link.style.animation) {
+//                 link.style.animation = '';
+//             } else {
+//                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+//             }
+//         });
+
+//         burger.classList.toggle('toggle');
+//     });
+// }
+
+// navSlide();
+
+function aboutpragraph() {
+    document.getElementById("about_pra").innerHTML =
+      "Quality, Passion, and Integrity - guide us in delivering the highest quality Parts and always doing what's right.";
+  }
+  function aboutpragraph1() {
+    document.getElementById("about_pra").innerHTML =
+      "At erum auto Craft, we believe in keeping things simple yet exceptional, with good Quality Producs  and a dedication to excellence at the core of what we do.";
+  }
+  
+  function aboutpragraph2() {
+    document.getElementById("about_pra").innerHTML =
+      "Our ultimate goal is to be recognized as the very best in the business, providing a unique and outstanding experience with every parts. We're working hard every day to establish ourselves as the top- quality parts  in the market.";
+  }
+  
